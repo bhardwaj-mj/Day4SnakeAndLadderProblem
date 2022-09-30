a484832500e2a8f9031236a01653e6635b3fc3e5
bhardwaj-mj <bhardwajmanoj555@gmail.com>
2022-09-30 20:43:56 +0530

    Added: Check options for no play,ladder and snake

--- src/com/bridgelabz/SnakeAndLadderProblem.java
+++ src/com/bridgelabz/SnakeAndLadderProblem.java
@@ -4,10 +4,24 @@ import java.util.Scanner;
 
 public class SnakeAndLadderProblem {
     static final int START_POSITION=0;
+    static final int NO_PLAY=0;
+    static final int LADDER=1;
+    static final int SNAKE=2;
     static int snakeLadder(){
         int player=START_POSITION;
         int dice=(int)Math.floor(Math.random()*10)%6+1;
-        player=dice;
+        int checkOption=(int)Math.floor(Math.random()*10)%3;
+        switch(checkOption){
+            case NO_PLAY:
+                player=player;
+                break;
+            case LADDER:
+                player=dice;
+                break;
+            case SNAKE:
+                player=player-dice;
+                break;
+        }
         return player;
     }
     public static void main(String[] args) {

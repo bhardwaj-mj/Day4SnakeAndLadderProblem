5c02e3481a963ea1bd10c668bebb24e0f5ca0b89
bhardwaj-mj <bhardwajmanoj555@gmail.com>
2022-09-30 23:14:55 +0530

    Added : How many times the dice is rolled

--- src/com/bridgelabz/SnakeAndLadderProblem.java
+++ src/com/bridgelabz/SnakeAndLadderProblem.java
@@ -8,6 +8,7 @@ public class SnakeAndLadderProblem {
     static int move=0;
     static int snakeLadder(){
         int player=START_POSITION;
+        int i=1;
         while(move<++move){
             int dice = (int) Math.floor(Math.random() * 10) % 6 + 1;
             int checkOption = (int) Math.floor(Math.random() * 10) % 3;
@@ -26,16 +27,19 @@ public class SnakeAndLadderProblem {
                 player=START_POSITION;
             }
             if(player>100){
-                player=player;
+                player=player-dice;
             }
             if(player==100)
                     break;
+            System.out.println("Position after "+i+" roll "+player);
+            i++;
             move++;
         }
         return player;
     }
     public static void main(String[] args) {
         int player=snakeLadder();
-        System.out.println(player);
+        System.out.println("Reached the winning position "+player);
+        System.out.println("Number of time dice rolled "+move/2);
     }
 }
